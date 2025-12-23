d023abecd2157c1ddb9b1e4c709cc8eb0100da9c
saranbabusarath-lab <saranbabusarath@gmail.com>
2025-12-23 14:58:30 +0530

    Add files via upload

---
+++ Principalammount.java
@@ -0,0 +1,21 @@
+class Principalammount
+{
+	public static void main(String[]args)
+		{
+			int si=24000;
+			int r=8;
+			int t=4;
+			 int p=(si*100/r*t);
+			  
+				System.out.println("principal ammount="+si);
+			  
+			
+			
+			
+			
+		}
+	
+	
+	
+	
+}
