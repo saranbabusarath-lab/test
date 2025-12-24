3d8a4c3ef8affdcc6f2797be0ae51be5bc014e87
saranbabusarath-lab <saranbabusarath@gmail.com>
2025-12-24 15:50:22 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,26 @@
+class Grade
+{
+	public static void main(String[]args)
+	{
+		int mark=100;
+		
+		if (mark>=90)
+			System.out.println("A grade");
+		else if(mark>=70)
+			System.out.println("B grade");
+		else if(mark>=50)
+			System.out.println("C grade");
+		else if(mark>=25)
+			System.out.println("D grade");
+		else
+			System.out.println("Fail");
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
---
+++ Vowelconsonant.java
@@ -0,0 +1,20 @@
+class Vowelconsonant
+{
+	public static void main(String[]args)
+	{
+		char kd='a';
+		if(kd=='a'||kd=='e'||kd=='i'||kd=='o'||kd=='u')
+			System.out.println(kd+"vowel");
+		else
+			System.out.println(kd+"consonant");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
